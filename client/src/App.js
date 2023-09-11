f4ed04705271b5c643b3cb125430004ad5148e0d
olgagrinko-dev <olgagrinko.87@mail.ru>
2023-09-11 22:15:41 +0300

    project

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import axios from 'axios';
-import {useState} from 'react';
+import { useEffect, useState } from 'react';
+import style from './app.module.css'
 
 function App() {
   const [obj, setObj] = useState('');
-  
-  async function show() {
-    const response = await axios.get(' https://api.ipify.org/?format=json');
-    console.log(response.data);
-    setObj(response.data.ip);
+  useEffect(() => {
+    send()
+  }, []);
+
+  async function send() {
+    const result = await axios.get('https://api.ipify.org/?format=json');
+    setObj(result.data.ip);
   }
 
   return (
-    <div className="App">
+    <div className={style.wrapper}>
       <h1>{obj}</h1>
-      <button onClick={show}>Click</button>
+      <p>37.215.53.63 ( This is your IP address...probably :P )</p>
     </div>
   );
 }
